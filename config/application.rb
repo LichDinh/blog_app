fbc71210a83ba662aa075f3f00fa63973f89cb58
lichdinhtb <lichdinhtb@gmail.com>
2018-09-18 10:45:21 +0700

    add micropots

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module BlogApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+    config.action_mailer.smtp_settings = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      domain:               "google.com",
+      user_name:           "awesome.academy.test@gmail.com",
+      password:             "Aa@123456",
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+  end
+end
